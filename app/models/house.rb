e6166a90934b6a15378fb588d3e866dfea8deea1
Timothee Garcia <grc.tmt@gmail.com>
2016-12-01 15:58:51 +0100

    search home

--- app/models/house.rb
+++ app/models/house.rb
@@ -5,6 +5,10 @@ class House < ApplicationRecord
 
   mount_uploader :photo, PhotoUploader
 
+  #GEOCODER
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
+
   validates :name,     presence: true
   validates :address,  presence: true
   validates :kind,     presence: true, inclusion: { in: ['full_house', 'dormitory', 'cat_tree'], allow_nil: false }
