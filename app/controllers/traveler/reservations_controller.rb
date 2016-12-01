411b95cf4b1d0146040268ed80f3e85ea96f6c4e
Pierre Vanoverschelde <pierre.vano@gmail.com>
2016-12-01 18:22:56 +0100

    creating rentals

--- app/controllers/traveler/reservations_controller.rb
+++ app/controllers/traveler/reservations_controller.rb
@@ -1,6 +1,11 @@
 class Traveler::ReservationsController < ApplicationController
   before_action :find_reservation, only: [:confirmation]
 
+  def index
+    @houses = current_cat.houses
+    @current_cat_reservations = current_cat.reservations
+  end
+
   def confirmation
   end
 
