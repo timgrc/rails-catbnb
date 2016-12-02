cd7c656b4d3a32d10011852f02158ddc48fe4281
Pierre Vanoverschelde <pierre.vano@gmail.com>
2016-12-02 11:48:47 +0100

    Rewire Devise resources

---
+++ app/helpers/pages_helper.rb
@@ -0,0 +1,13 @@
+module PagesHelper
+  def resource_name
+    @resource_name = :cat
+  end
+
+  def resource
+    @resource ||= resource_name.to_s.classify.constantize.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[resource_name]
+  end
+end
